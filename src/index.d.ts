deae60cad86db3a6abae4d3f219c46aa4bfd196b
Amit Kumar <amit.kumar@snappcar.nl>
2020-07-19 10:24:14 +0200

    initial code commit

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './timepicker.common';
+export declare class Timepicker extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
---
+++ src/timepicker.ios.d.ts
@@ -0,0 +1,3 @@
+import { Common } from './timepicker.common';
+export declare class Timepicker extends Common {
+}
